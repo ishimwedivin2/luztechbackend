74b4daa0c275d773dfacd1d8cf6460ff26be195e
Ishimwe Divin <ishimwedivin2@gmail.com>
2026-02-16 19:28:32 +0200

    first commit

---
+++ src/test/java/com/luz/tech/TechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luz.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
